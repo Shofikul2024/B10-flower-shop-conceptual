c8e7ed9fa53d3851b3294b6a3fb25e5baa37a8fb
Shofikul2024 <islamshofi942@gmail.com>
2025-02-17 12:35:39 +0600

    added  div  part

--- arrow.js
+++ arrow.js
@@ -1,9 +1,16 @@
- 
+//  using arrow function   division  two number . 
+
+const division =( num1 ,num2)=> num1/num2
+const result=division( 100 ,5)
+console.log(result)
+
+
+
 
 //  using arrow function  subtraction  two number . 
 
 
 
-const subtract= ( num1 ,num2) => num1 -num2
-const result =( 20,10)
-console.log(result)
+// const subtract= ( num1 ,num2) => num1 -num2
+// const result =( 20,10)
+// console.log(result)
