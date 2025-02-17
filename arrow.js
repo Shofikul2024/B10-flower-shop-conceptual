96e9c42c6dab8c1001e2a480d8814f023397b7f9
Shofikul2024 <islamshofi942@gmail.com>
2025-02-17 12:37:49 +0600

    added  mul  part

--- arrow.js
+++ arrow.js
@@ -1,8 +1,8 @@
 //  using arrow function   division  two number . 
 
-const division =( num1 ,num2)=> num1/num2
-const result=division( 100 ,5)
-console.log(result)
+// const division =( num1 ,num2)=> num1/num2
+// const result=division( 100 ,5)
+// console.log(result)
 
 
 
@@ -12,5 +12,16 @@ console.log(result)
 
 
 // const subtract= ( num1 ,num2) => num1 -num2
-// const result =( 20,10)
+// const result = ( 20,10)
 // console.log(result)
+
+
+
+
+//  using arrow function  multiply two number . 
+
+
+
+const multiply= ( num1 ,num2) => num1 *num2
+const result = multiply( 20,10)
+console.log(result)
