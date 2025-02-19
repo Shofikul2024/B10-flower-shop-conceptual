1b2d1e3298c4dffb2da27a3aacb9dff5d2d0d2d5
Shofikul2024 <islamshofi942@gmail.com>
2025-02-19 16:27:34 +0600

    added nested loop   looping

--- loop.js
+++ loop.js
@@ -1,12 +1,13 @@
-// for (let i=1 ; i<= 5; i++ )
-// {
-// console.log("hello from  i")
+for (let i=1 ; i<= 5; i++ )
+{
+console.log("hello from  i")
 
 
-// }
+
 
   for  ( let j=1 ;  j<= 10  ; j++)
   {
      console.log("shofikul likha ti 10 bar  print  koro")
-     
+  }
+
   }
